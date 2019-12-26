fd7c9279b534508b6747998c9e523c862d1d2535
BlockJJam <blockjjam99@gmail.com>
2019-12-26 18:10:29 +0900

    This update is IPC's Kernel Object Share and Data Share Between Process

---
+++ Windows_System_Programming_IPC/AtlServiceTest/AtlServiceTest.cpp
@@ -0,0 +1,80 @@
+﻿// AtlServiceTest.cpp : WinMain 구현입니다.
+
+
+#include "stdafx.h"
+#include "resource.h"
+#include "AtlServiceTest_i.h"
+
+
+using namespace ATL;
+
+#include <stdio.h>
+
+class CAtlServiceTestModule : public ATL::CAtlServiceModuleT< CAtlServiceTestModule, IDS_SERVICENAME >
+{
+	HANDLE m_hevExit;
+
+public :
+	DECLARE_LIBID(LIBID_AtlServiceTestLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_ATLSERVICETEST, "{e35bfda0-4ff4-4194-ad01-3cf55a5c54f8}")
+	HRESULT InitializeSecurity() throw()
+	{
+		// TODO : CoInitializeSecurity를 호출하고 서비스에 올바른 보안 설정을 적용하십시오.
+		// 제안 - PKT 수준 인증,
+		// RPC_C_IMP_LEVEL_IDENTIFY의 가장 수준
+		// 및 적절한 Null이 아닌 보안 설명자
+
+		return S_OK;
+	}
+
+	void OnStop();
+
+	HRESULT PreMessageLoop(int nShorCmd);
+	HRESULT PostMessageLoop() throw();
+	void RunMessageLoop() throw();
+
+};
+
+CAtlServiceTestModule _AtlModule;
+
+
+
+
+//
+extern "C" int WINAPI _tWinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/,
+								LPTSTR /*lpCmdLine*/, int nShowCmd)
+{
+	return _AtlModule.WinMain(nShowCmd);
+}
+
+void CAtlServiceTestModule::OnStop()
+{
+	SetEvent(m_hevExit);
+	__super::OnStop();
+}
+
+HRESULT CAtlServiceTestModule::PreMessageLoop(int nShowCmd)
+{
+	m_hevExit = CreateEvent(NULL, true, false, NULL);
+	if (m_hevExit == NULL)
+		return HRESULT_FROM_WIN32(GetLastError());
+
+	if (::InterlockedCompareExchange(&m_status.dwCurrentState, SERVICE_RUNNING, SERVICE_START_PENDING) == SERVICE_START_PENDING)
+	{
+		LogEvent(_T("Servcie started/resumed"));
+		::SetServiceStatus(m_hServiceStatus, &m_status);
+	}
+	return S_OK;
+}
+
+HRESULT CAtlServiceTestModule::PostMessageLoop()
+{
+	if (m_hevExit != NULL)
+		CloseHandle(m_hevExit);
+	return __super::PostMessageLoop();
+}
+
+void CAtlServiceTestModule::RunMessageLoop()
+{
+	WaitForSingleObject(m_hevExit, INFINITE);
+}
---
+++ Windows_System_Programming_IPC/IPC/WaitsForInputIdle.cpp
@@ -0,0 +1,42 @@
+#include "stdafx.h"
+#include "windows.h"
+#include "Shlwapi.h"
+#include "iostream"
+
+using namespace std;
+
+#pragma comment(lib, "Shlwapi.lib")
+
+void _tmain(void)
+{
+	cout << " ===== Start waitforinputidle test ========= " << endl;
+
+	TCHAR szRunCmdLine[MAX_PATH] = { 0. };
+	DWORD dwPathLen = ::GetModuleFileName(NULL, szRunCmdLine, MAX_PATH);
+	::PathRemoveFileSpec(szRunCmdLine);
+	_tcscat_s(szRunCmdLine, _T("\\TestGUIApp.exe"));
+
+	STARTUPINFO si;
+	ZeroMemory(&si, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);
+	PROCESS_INFORMATION pi;
+	ZeroMemory(&pi, sizeof(pi));
+
+	BOOL bIsOK = CreateProcess
+	(
+		NULL, szRunCmdLine, NULL, NULL, false, 0, NULL, NULL, &si, &pi
+	);
+	if (!bIsOK)
+	{
+		cout << "CreateProcess failed : " << GetLastError() << endl;
+		return;
+	}
+
+	DWORD dwWaitCode = WaitForInputIdle(pi.hProcess, INFINITE);
+	if (dwWaitCode == WAIT_FAILED)
+		cout << "WaitForInputIdle failed : " << GetLastError() << endl;
+	else
+		cout << "TestGUIApp.exe initialization completed .... " << endl;
+	CloseHandle(pi.hThread);
+	CloseHandle(pi.hProcess);
+}
